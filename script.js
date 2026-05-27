405aa85c0fe66a413f4628fc96e45f71520fd555
Akash Agrawal <akash1997agrawal@gmail.com>
2026-05-27 11:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-const container = document.getElementById('puzzle-container');
+const container = document.getElementById("puzzle-container");
+
+const positions = [
+  "0px 0px",
+  "-100px 0px",
+  "-200px 0px",
+  "0px -100px",
+  "-100px -100px",
+  "-200px -100px",
+  "0px -200px",
+  "-100px -200px",
+  "-200px -200px"
+];
 
 let shuffled = [...positions].sort(() => Math.random() - 0.5);
 
 function renderPuzzle() {
-  container.innerHTML = '';
+  container.innerHTML = "";
 
   shuffled.forEach((pos, index) => {
-    const piece = document.createElement('div');
-    piece.classList.add('piece');
+    const piece = document.createElement("div");
+
+    piece.classList.add("piece");
     piece.style.backgroundPosition = pos;
     piece.draggable = true;
     piece.dataset.index = index;
 
-    piece.addEventListener('dragstart', dragStart);
-    piece.addEventListener('dragover', dragOver);
-    piece.addEventListener('drop', drop);
+    piece.addEventListener("dragstart", dragStart);
+    piece.addEventListener("dragover", dragOver);
+    piece.addEventListener("drop", drop);
 
     container.appendChild(piece);
   });
 }
 
-let draggedIndex = null;
+let dragged = null;
 
 function dragStart() {
-  draggedIndex = this.dataset.index;
+  dragged = this.dataset.index;
 }
 
 function dragOver(e) {
@@ -31,22 +44,29 @@ function dragOver(e) {
 }
 
 function drop() {
-  const targetIndex = this.dataset.index;
+  const target = this.dataset.index;
 
-  [shuffled[draggedIndex], shuffled[targetIndex]] =
-  [shuffled[targetIndex], shuffled[draggedIndex]];
+  [shuffled[dragged], shuffled[target]] =
+    [shuffled[target], shuffled[dragged]];
 
   renderPuzzle();
   checkWin();
 }
 
 function checkWin() {
-  const win = positions.every((pos, i) => pos === shuffled[i]);
+  let win = true;
+
+  for (let i = 0; i < positions.length; i++) {
+    if (positions[i] !== shuffled[i]) {
+      win = false;
+      break;
+    }
+  }
 
   if (win) {
     setTimeout(() => {
-      window.location.href = 'final.html';
-    }, 800);
+      window.location.href = "final.html";
+    }, 500);
   }
 }
 
